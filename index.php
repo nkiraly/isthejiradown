539bcd3b2993044a85f89f77818ecdeb0c046702
Nicholas Kiraly <kiraly.nicholas@gmail.com>
2014-03-13 09:58:39 -0400

    add unavailability reason templating

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
 <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<?php
+  $fuck_up_reasons = array(
+    'original' => array(
+      'title' => "503 Service Temporarily Unavailable",
+      'heading' => "Service Temporarily Unavailable",
+      'message' => "The server is temporarily unable to service your request due to maintenance downtime or capacity problems. Please try again later."
+    )
+  );
+  $fuck_up_reason_index = array_keys($fuck_up_reasons);
+  $fuck_up_panicked_answer_index = rand(0, count($fuck_up_reason_index) - 1);
+  $fuck_up_panicked_answer_key = $fuck_up_reason_index[$fuck_up_panicked_answer_index];
+  $fuck_up_bullshit_excuse_du_jour = $fuck_up_reasons[$fuck_up_panicked_answer_key];
+?>
 <html><head>
-<title>503 Service Temporarily Unavailable</title>
+<title><?php echo $fuck_up_bullshit_excuse_du_jour['title']; ?></title>
 </head><body>
-<h1>Service Temporarily Unavailable</h1>
-<p>The server is temporarily unable to service your
-request due to maintenance downtime or capacity
-problems. Please try again later.</p>
+<h1><?php echo $fuck_up_bullshit_excuse_du_jour['heading']; ?></h1>
+<p><?php echo $fuck_up_bullshit_excuse_du_jour['message']; ?></p>
 </body></html>
