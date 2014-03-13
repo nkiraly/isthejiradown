8bdd033c1bfd5b61676b9d0c14c9462d19016ba5
Nicholas Kiraly <kiraly.nicholas@gmail.com>
2014-03-13 18:13:26 -0400

    always show the fuck your shit reason. isn't that why we're all looking at this page?

--- index.php
+++ index.php
@@ -2,29 +2,25 @@
 <?php
   $fuck_your_shit_reasons = array(
   	"And oh yeah, fuck you.",
-  	"Oh, and fuck your ticket edit."
+  	"Oh, and fuck your ticket edit.",
+    "That edit was cray, brah. Rethink yourself."
   );
   $fuck_your_shit_reason = $fuck_your_shit_reasons[rand(0, count($fuck_your_shit_reasons) - 1)];
   $fuck_up_reasons = array(
-    'unavailable' => array(
-      'title' => "503 Service Temporarily Unavailable",
-      'heading' => "Service Temporarily Unavailable",
-      'message' => "The server is temporarily unable to service your request due to maintenance downtime or capacity problems. Please try again later."
+    'internal_error_rage' => array(
+      'title' => "500 Internal Server Error",
+      'heading' => "Internal Server Error",
+      'message' => "Something inside the server is making an awful clanging. Better call Sean."
     ),
     'unavailable_rage' => array(
       'title' => "503 Service Temporarily Unavailable",
       'heading' => "Service Temporarily Unavailable",
-      'message' => "The server is temporarily unable to service your request due to maintenance downtime or capacity problems. Please try again later. " . $fuck_your_shit_reason
-    ),
-    'bad_gateway' => array(
-      'title' => "502 Bad Gateway",
-      'heading' => "Bad Gateway",
-      'message' => "The proxy server recieved and invalid response from and upstream server."
+      'message' => "The server is temporarily unable to service your request due to maintenance downtime or capacity problems. Please try again later."
     ),
     'bad_gateway_rage' => array(
       'title' => "502 Bad Gateway",
       'heading' => "Bad Gateway",
-      'message' => "The proxy server recieved and invalid response from and upstream server. " . $fuck_your_shit_reason
+      'message' => "The proxy server recieved and invalid response from and upstream server."
     ),
   );
   $fuck_up_reason_index = array_keys($fuck_up_reasons);
@@ -32,9 +28,11 @@
   $fuck_up_panicked_answer_key = $fuck_up_reason_index[$fuck_up_panicked_answer_index];
   $fuck_up_bullshit_excuse_du_jour = $fuck_up_reasons[$fuck_up_panicked_answer_key];
 ?>
-<html><head>
+<html>
+<head>
 <title><?php echo $fuck_up_bullshit_excuse_du_jour['title']; ?></title>
 </head><body>
 <h1><?php echo $fuck_up_bullshit_excuse_du_jour['heading']; ?></h1>
 <p><?php echo $fuck_up_bullshit_excuse_du_jour['message']; ?></p>
+<p><?php echo $fuck_your_shit_reason; ?></p>
 </body></html>
