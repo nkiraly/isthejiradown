461b9915c7001f7e3e3f176522e475ef04219b62
Nicholas Kiraly <kiraly.nicholas@gmail.com>
2014-03-13 18:23:36 -0400

    add google analytics to the jiras

--- index.php
+++ index.php
@@ -31,6 +31,18 @@
 <html>
   <head>
     <title><?php echo $fuck_up_bullshit_excuse_du_jour['title']; ?></title>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-48981517-2', 'isthejiradown.com');
+  ga('send', 'pageview');
+
+</script>
+
   </head>
   <body>
     <h1><?php echo $fuck_up_bullshit_excuse_du_jour['heading']; ?></h1>
